4561dde8df66913699439af9ac01f8669633ee29
Ivanin11 <jiavanalarcon@yahoo.es>
2021-05-27 20:42:10 +0200

    se crea el test de la operación suma

---
+++ test/suma.test.js
@@ -0,0 +1,11 @@
+const suma = require("../src/suma");
+
+describe("test de la operación suma", () => {
+    test('el resultado de sumar -3 + 2 = -1', () => {
+        const a = -3;
+        const b = 2;
+        const resultado_esperado = -1;
+        const resultado_funcion = suma(a, b);
+        expect(resultado_funcion).toBe(resultado_esperado);
+    })
+});
